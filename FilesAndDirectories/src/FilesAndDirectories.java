3e9da8fcbc3075bf6551023eef2a9b17c5e57c4a
KostadinPlachkov <kostt23@gmail.com>
2017-06-20 00:52:55 +0300

    Added: File, path

--- FilesAndDirectories/src/FilesAndDirectories.java
+++ FilesAndDirectories/src/FilesAndDirectories.java
@@ -1,4 +1,10 @@
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
 
 /**
  * Created by Kostadin on 19-Jun-17.
@@ -9,6 +15,7 @@ public class FilesAndDirectories {
         String inputFile = "input.txt";
         String outputFile = "output.txt";
 
+        // Streams
         try (FileInputStream fileInputStream = new FileInputStream(inputFile);
              FileOutputStream fileOutputStream = new FileOutputStream(outputFile)) {
             int oneByte = fileInputStream.read();
@@ -19,6 +26,30 @@ public class FilesAndDirectories {
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        try (Scanner scanner = new Scanner(new FileInputStream(inputFile));
+            PrintWriter printWriter = new PrintWriter(outputFile)) {
+            while (scanner.hasNext()) {
+                printWriter.println(scanner.next());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Files and path
+        Path pathIn = Paths.get(inputFile);
+        Path pathOut = Paths.get(outputFile);
+        try {
+            List<String> lines = Files.readAllLines(pathIn);
+            Collections.sort(lines);
+            Files.write(pathOut, lines);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        File file = new File(inputFile);
+
+        // Serialization
 
     }
 }
